90478c7452c1eb3124537a6d5532df80ba391402
xuhang57 <xuh@bu.edu>
2015-04-26 19:32:47 -0400

    add more classes for retrieving team stats

--- nbastats/nbastats.py
+++ nbastats/nbastats.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+
 pd.set_option('notebook_repr_html',True)
 pd.set_option('display.max_columns',300)
 pd.set_option('display.width',3000)
@@ -41,7 +42,23 @@ class ShotChart:
     def leagueaverage(self):
         return pd.DataFrame(self._x['resultSets'][1]['rowSet'],columns=self._x['resultSets'][1]['headers'])
 
-class Team:
+class Team_info:
+    def __init__(self, teamid, leagueid='00',season='2013-14',seasontype='Regular Season',
+              vsconference='',vsdivision=''):
+
+        self._url = "http://stats.nba.com/stats/teaminfocommon?"
+        self._api_param = {
+             'LeagueID': leagueid,           
+             'Season' :  season,
+             'SeasonType' : seasontype,
+             'TeamID' : teamid
+             }
+        self._x = requests.get(self._url, params=self._api_param)
+        self._x = self._x.json()
+    def info(self):
+        return pd.DataFrame(self._x['resultSets'][0]['rowSet'],columns=self._x['resultSets'][0]['headers']) 
+
+class Team_details:
     def __init__(self, teamid, datefrom ='', dateto ='', gamesegment ='',lastngames =0, leagueid='00',location='',measuretype ='Base',month=0,opponentteamid=0,outcome='',
               paceadjust='N',permode='PerGame',period=0,plusminus='N',rank='N',season='2013-14',seasonsegment='',seasontype='Regular Season',
               vsconference='',vsdivision=''):
@@ -74,9 +91,36 @@ class Team:
         self._x = self._x.json()
     def overall(self):
         return pd.DataFrame(self._x['resultSets'][0]['rowSet'],columns=self._x['resultSets'][0]['headers']) 
-    # def leagueaverage(self):
-    #     return pd.DataFrame(self._x['resultSets'][1]['rowSet'],columns=self._x['resultSets'][1]['headers'])
 
+
+class PlayerBio:
+    def __init__(self, playerid, leagueid = '00'):
+
+        self._url = "http://stats.nba.com/stats/commonplayerinfo?"
+        self._api_param = {
+             'PlayerID'  : playerid,
+             'LeagueID': leagueid     
+             
+             }
+        self._x = requests.get(self._url, params=self._api_param)
+        self._x = self._x.json()
+    def bio(self):
+        return pd.DataFrame(self._x['resultSets'][0]['rowSet'],columns=self._x['resultSets'][0]['headers'])
+
+class PlayerStats:
+    def __init__(self, playerid, leagueid = '00', permode='PerGame'):
+
+        self._url = "http://stats.nba.com/stats/playercareerstats?"
+        self._api_param = {
+             'PlayerID': playerid,
+             'LeagueID': leagueid,     
+             'PerMode' : permode
+             
+             }
+        self._x = requests.get(self._url, params=self._api_param)
+        self._x = self._x.json()
+    def careerstats(self):
+        return pd.DataFrame(self._x['resultSets'][1]['rowSet'],columns=self._x['resultSets'][1]['headers']) 
 
 
 class BoxScore:
